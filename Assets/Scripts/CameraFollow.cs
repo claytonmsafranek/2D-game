f46743713ed5b6dd5df54b49576500d27ad79eea
Clayton Safranek <claytonsafranek@gmail.com>
2023-07-20 15:02:01 -0500

    player bounds set, camera follow done

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float speed;
+
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            float clampedX = Mathf.Clamp(playerTransform.position.x, minX, maxX);
+            float clampedY = Mathf.Clamp(playerTransform.position.y, minY, maxY);
+
+            transform.position = Vector2.Lerp(transform.position, new Vector2(clampedX, clampedY), speed);
+        }
+    }
+
+}
