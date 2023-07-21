a04a6b0345c5988a77e8ca229d52790ba77a3c6d
Clayton Safranek <claytonsafranek@gmail.com>
2023-07-21 15:03:07 -0500

    basic enemy made

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            // enemy is dead, destory it
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
